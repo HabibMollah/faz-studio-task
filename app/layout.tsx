e20cb5804fb2d1ce4a4194355da8c9e73f0328d7
HabibMollah <habib.prodev@gmail.com>
2023-10-12 10:02:33 +0600

    add font and background and foreground color

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import "./globals.scss";
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 
-const manrope = Manrope({ subsets: ["latin"] });
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
