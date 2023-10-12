ba8190e0f29c6e9d95c15c71d2756001b751733f
HabibMollah <habib.prodev@gmail.com>
2023-10-13 00:31:03 +0600

    populate navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,35 @@
+import Icon from "./Icon";
+
 export default function Navbar() {
   return (
-    <header className="fixed bottom-0 bg-black text-white">
-      <nav>Navbar</nav>
+    <header className="fixed bottom-0 w-screen bg-[#f7f7f7] px-4 py-3 font-bold text-black">
+      <nav className="mx-auto flex max-w-[1344px] items-center">
+        {/* Cancel button */}
+        <button className="flex items-center gap-4 border border-[#4B4B4B] bg-white p-4">
+          <Icon variant="cross.svg" />
+          <span className="sr-only">Cancel/Close</span>
+        </button>
+        {/* Text contents */}
+        <div className="flex justify-evenly text-xl">
+          <p className="px-8">Preview listing</p>
+          <p className="px-8">150/12 Orchard Cottage, London 56273</p>
+        </div>
+        {/* Right side buttons */}
+        <div className="ml-auto flex gap-4">
+          <button className="flex items-center gap-4 border border-[#4B4B4B] bg-white p-4">
+            <Icon variant="copy.svg" />
+            Save draft
+          </button>
+          <button className="flex items-center gap-4 bg-white p-4 shadow-[0px_16px_40px_-7px_#00000005]">
+            Edit
+            <Icon variant="edit.svg" />
+          </button>
+          <button className="flex items-center gap-4 bg-black p-4 text-white">
+            Publish
+            <Icon variant="chevron-right.svg" />
+          </button>
+        </div>
+      </nav>
     </header>
   );
 }
