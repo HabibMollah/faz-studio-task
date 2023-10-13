768cbae66f2c089a7e5141de46fac2bb80dc43f2
HabibMollah <habib.prodev@gmail.com>
2023-10-13 17:31:19 +0600

    make input component more reusable

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,20 +1,32 @@
 type Props = {
-  label: string;
+  label?: string;
   type: string;
-  placeholder: string;
+  placeholder?: string;
+  value?: string | number;
+  disabled?: boolean;
+  className?: string;
 };
 
-export default function Input({ label, type, placeholder }: Props) {
+export default function Input({
+  label,
+  type,
+  placeholder,
+  value,
+  disabled,
+  className,
+}: Props) {
   return (
     <div className="flex max-w-[312px] flex-col gap-2">
       <label className="text-sm font-[700]" htmlFor={label}>
         {label}
       </label>
       <input
-        className="w-full px-6 py-4 text-xl"
+        className={`w-full px-6 py-4 text-xl ${className}`}
         id={label}
         type={type}
         placeholder={placeholder}
+        value={value}
+        disabled={disabled}
       />
     </div>
   );
