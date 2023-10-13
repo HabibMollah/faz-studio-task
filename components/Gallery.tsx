d080918a9519af2a895b1a1b4f2452d4f4482d5f
HabibMollah <habib.prodev@gmail.com>
2023-10-13 08:16:53 +0600

    animate gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 const imgSrcList = [
@@ -8,18 +10,37 @@ const imgSrcList = [
   "shower.webp",
 ];
 
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: -100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.2 * index,
+    },
+  }),
+};
+
 export default function Gallery() {
   return (
     <section className="mx-auto flex items-center justify-center">
       <ul className="grid grid-cols-2 gap-1 xl:grid-cols-[600px,368px,368px] xl:grid-rows-2">
         {imgSrcList.map((src, index) => (
-          <li
+          <motion.li
             className={`${
               index === 0
                 ? "col-span-2 md:col-span-2 md:aspect-square xl:col-span-1 xl:row-span-2"
                 : "aspect-square md:aspect-auto md:h-[298px] md:w-[368px]"
             }`}
             key={src}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
           >
             <Image
               className="h-full w-full object-cover"
@@ -28,7 +49,7 @@ export default function Gallery() {
               width={500}
               height={500}
             />
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
