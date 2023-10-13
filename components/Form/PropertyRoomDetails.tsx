255bc0d66d2bc1a76d35493c87b98bd2861e4586
HabibMollah <habib.prodev@gmail.com>
2023-10-13 17:31:07 +0600

    add box shadow

--- components/Form/PropertyRoomDetails.tsx
+++ components/Form/PropertyRoomDetails.tsx
@@ -7,16 +7,38 @@ export default function PropertyRoomDetails() {
         Property /Room Details
       </h2>
       <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-3">
-        <Input label="Property Type" type="text" placeholder="e.g. Room Only" />
-        <Input label="Number of Bedrooms" type="number" placeholder="e.g. 3" />
-        <Input label="Number of Bathrooms" type="number" placeholder="e.g. 3" />
         <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
+          label="Property Type"
+          type="text"
+          placeholder="e.g. Room Only"
+        />
+        <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
+          label="Number of Bedrooms"
+          type="number"
+          placeholder="e.g. 3"
+        />
+        <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
+          label="Number of Bathrooms"
+          type="number"
+          placeholder="e.g. 3"
+        />
+        <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
           label="Furnishing Options"
           type="text"
           placeholder="e.g. Not Furnished"
         />
-        <Input label="Area" type="text" placeholder="e.g. 1500 sqft" />
         <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
+          label="Area"
+          type="text"
+          placeholder="e.g. 1500 sqft"
+        />
+        <Input
+          className="shadow-[0px_28px_55px_-16px_#d3d4d42b]"
           label="EPC Rating (Purchase EPC for $69)"
           type="number"
           placeholder="e.g. 23"
