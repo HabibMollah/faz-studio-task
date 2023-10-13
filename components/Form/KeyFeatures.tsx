f13795246933e8253052981df4dbf19c2c0429b0
HabibMollah <habib.prodev@gmail.com>
2023-10-13 17:38:02 +0600

    finish keyfeatures section

--- components/Form/KeyFeatures.tsx
+++ components/Form/KeyFeatures.tsx
@@ -6,32 +6,36 @@ export default function KeyFeatures() {
       <h2 className="min-w-[312px] text-2xl font-bold tracking-[0.24px]">
         Key Features
       </h2>
-      <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-3">
+      <div className="grid w-full max-w-[656px] grid-cols-1 gap-8 !font-bold sm:grid-cols-2">
         <Input
           type="text"
           disabled
-          className="shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          className="!text-sm shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
           value={"Bills Included"}
         />
         <Input
           type="text"
           disabled
-          className="shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          className="!text-sm shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          value={"Garden Access"}
         />
         <Input
           type="text"
           disabled
-          className="shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          className="!text-sm shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          value={"Parking"}
         />
         <Input
           type="text"
           disabled
-          className="shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          className="!text-sm shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          value={"Lift"}
         />
         <Input
           type="text"
           disabled
-          className="shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          className="!text-sm shadow-[0px_16px_40px_-7px_#0000000d] disabled:bg-white"
+          value={"Fireplace"}
         />
       </div>
     </div>
