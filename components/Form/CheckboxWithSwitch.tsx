7cdc11bb51ce9c22598aa9eb3d7dcd41cd3463fa
HabibMollah <habib.prodev@gmail.com>
2023-10-14 10:49:37 +0600

    fine tune switch

--- components/Form/CheckboxWithSwitch.tsx
+++ components/Form/CheckboxWithSwitch.tsx
@@ -8,7 +8,6 @@ export default function CheckboxWithSwitch({ label, className }: Props) {
   return (
     <div className="flex max-h-[48px] max-w-[312px] flex-row items-center justify-between gap-2 bg-white px-8 py-4 shadow-[0px_16px_40px_-7px_#0000000d] lg:flex-row">
       <p className="text-sm font-[700] text-black">{label}</p>
-      {/* <div className={`w-full px-6 py-4 text-xl ${className}`}></div> */}
       <Switch />
     </div>
   );
